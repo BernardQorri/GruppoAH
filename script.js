ef07d54c56edf4cea6bda911722796e1946318d7
BernardQorri <bernardhysenaj0@gmail.com>
2026-03-26 12:03:16 +0100

    CommitPerPush

---
+++ script.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([41.8902, 12.4922], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
